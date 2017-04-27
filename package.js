fc2d927d86e90246ad23708278052f3f0f14d769
Fabian Kromer <fabian.kromer@gmail.com>
2017-04-27 15:22:44 +0200

    Initial commit of a working Cloudron accounts integration

---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: 'kromit:accounts-cloudron',
+  version: '0.0.1',
+  summary: 'OAuth2 for Cloudron',
+  git: 'https://github.com/faburem/accounts-cloudron',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.use(['underscore', 'random']);
+  api.use('accounts-base', ['client', 'server']);
+  // Export Accounts (etc) to packages using this one.
+  api.imply('accounts-base', ['client', 'server']);
+
+  api.use('accounts-oauth', ['client', 'server']);
+  api.use('kromit:cloudron', ['client', 'server']);
+  
+  api.addFiles('accounts-cloudron.js');
+});
